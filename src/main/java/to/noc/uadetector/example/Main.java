fe2e935411e15c4c58412b02de38d2734cdd5b41
Дмитрий Холодов <github@sec.to>
2013-02-15 00:56:45 -0700

    reordered output and added ua name

--- src/main/java/to/noc/uadetector/example/Main.java
+++ src/main/java/to/noc/uadetector/example/Main.java
@@ -14,29 +14,31 @@ public class Main {
         "Mozilla/5.0 (Windows NT 6.1; rv:16.0) Gecko/20100101 Firefox/16.0",
         "Opera/9.80 (Windows NT 6.1; WOW64; U; en) Presto/2.10.289 Version/12.01",
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.101 Safari/537.11",
+        "Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)",
         "Bogus User Agent"
     };
 
     public static void printUserAgent(UserAgent ua) {               
         StringBuilder sb = new StringBuilder();
         
-        sb.append("Type: ").append(ua.getTypeName()).append("\n");
-        sb.append("Family: ").append(ua.getFamily()).append("\n");
         sb.append("Name: ").append(ua.getName()).append("\n");
         sb.append("Version: ").append(ua.getVersionNumber().toVersionString()).append("\n");
-        sb.append("Producer URL: ").append(ua.getProducerUrl()).append("\n");
-        sb.append("Producer: ").append(ua.getProducer()).append("\n");
+        sb.append("Type: ").append(ua.getTypeName()).append("\n");
+        sb.append("Family: ").append(ua.getFamily()).append("\n");
         sb.append("URL: ").append(ua.getUrl()).append("\n");
         sb.append("Icon: ").append(ua.getIcon()).append("\n");
+        sb.append("Producer: ").append(ua.getProducer()).append("\n");
+        sb.append("Producer URL: ").append(ua.getProducerUrl()).append("\n");
         
         OperatingSystem os = ua.getOperatingSystem();
-        sb.append("OS Company: ").append(os.getProducer()).append("\n");
-        sb.append("OS Company URL: ").append(os.getProducerUrl()).append("\n");
+        sb.append("OS Name: ").append(os.getName()).append("\n");
         sb.append("OS Family Name: ").append(os.getFamilyName()).append("\n");
         sb.append("OS Version Number: ").append(os.getVersionNumber().toVersionString()).append("\n");
         sb.append("OS URL: ").append(os.getUrl()).append("\n");
         sb.append("OS Icon: ").append(os.getIcon()).append("\n");
-
+        sb.append("OS Company: ").append(os.getProducer()).append("\n");
+        sb.append("OS Company URL: ").append(os.getProducerUrl()).append("\n");
+        
         System.out.println(sb);        
     }
    
